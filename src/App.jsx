bd640285212cbdd80435d8a40e6da9d43881ac9c
Divanshu Bhargava <divanshu0212@gmail.com>
2025-02-02 15:23:05 +0530

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Welcome from './pages/welcome';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import GameList from './pages/GameList'
 import GameDetails from './pages/GameDetails';
-import { HashRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <HashRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/login" element={<Login />} />
@@ -17,7 +16,7 @@ function App() {
         <Route path="/home" element={<GameList />} />
         <Route path="/game/:id" element={<GameDetails />} />
       </Routes>
-    </HashRouter>
+    </Router>
   );
 }
 
